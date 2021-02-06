dbbc72e0863ebf91e3517ddc8fb73c4b38dbbe44
Atonok <66049081+Atonok@users.noreply.github.com>
2021-02-06 03:36:04 +0300

    first commit

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
-print("Sos")
-# че каво?
-Все
-гуд?
+import requests as req
+from bs4 import BeautifulSoup
+
+
+def track_now():
+    resp = req.get("https://onlineradiobox.com/ru/svoefm/playlist/?cs=ru.svoefm")
+    soup = BeautifulSoup(resp.text, 'lxml')
+    track_now1 = soup.find('tr').a.string
+
+    return f'{track_now1}.mp3'
+
+
+def load_track():
+    r = req.get("https://r51-15-210-236.relay.radiotoolkit.com:30003/svoefm", stream=True)
+
+    while True:
+        track_name = track_now()
+        print(track_name)
+
+        with open(track_name, 'ab') as f:
+            for block in r.iter_content(131072):
+                if track_now() == track_name:
+                    f.write(block)
+                else:
+                    break
+
+
+if __name__ != "__main__":
+    load_track()
